6004d87469176ef9dd14c6f17f5e5ea0ed45e7df
regilanmeira <regilan@hotmail.com>
2025-10-06 10:39:43 -0300

    Finalizado o Inserir

--- tarefa_inserir.php
+++ tarefa_inserir.php
@@ -9,6 +9,43 @@
     ?>
 </head>
 <body>
+    <?php
+        //OPERAÇÃO DE INSERT 
+        //1º PASSO: CAPTURAR CADA REGISTRO DEVE SER INSERIDO NA TABELA
+        // INSERT INTO () VALUES ()
+
+        $descricao = $_POST["txtDescricao"];
+        $data_entrega = $_POST["txtData"];
+        $prioridade = $_POST["txtPrioridade"];
+        $responsavel = $_POST["txtResponsavel"];
+
+        //2º PREPARAR O COMANDO SQL QUE SERÁ EXECUTADO
+        //Criamos uma variável e passamos os parametros como (?)
+        //Cada parametro ficará com Interrogação (?)
+
+        $sql = "INSERT INTO tarefa(descricao,data_entrega,prioridade,responsavel) VALUES (?,?,?,?)";
+
+        //3º VINCULAR ONDE O COMANDO SQL SERÁ EXECUTADO
+        $comando = $conexao->prepare($sql);
+
+        //4º ASSOCIAR CADA (?) COM SEUS VALORES RESPECTIVOS
+        $comando->bind_param("ssss",$descricao,$data_entrega,$prioridade,$responsavel);
+
+        //5º EXECUTAR O COMANDO NA CONEXÃO DE DADOS
+
+        if ($comando->execute())
+        {
+            echo "<h1>Tarefa marcada!</h1>";
+        }
+        else
+        {
+            echo "<h1>Erro!Confira os dados!</h1>";
+        }
+
+
+
+
+    ?>
   
     
 </body>
